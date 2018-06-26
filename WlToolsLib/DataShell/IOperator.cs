24be37a6ce616286608967d40199285f71be4fe7
wlfsky <raddleoj@sina.com>
2018-06-26 23:44:12 +0800

    基本操作人信息

---
+++ WlToolsLib/DataShell/IOperator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WlToolsLib.DataShell
+{
+    /// <summary>
+    /// 操作人信息
+    /// </summary>
+    public interface IOperator
+    {
+        /// <summary>
+        /// 操作人id
+        /// </summary>
+        string OperatorID { get; set; }
+
+        /// <summary>
+        /// 操作人名
+        /// </summary>
+        string OperatorName { get; set; }
+
+    }
+
+    /// <summary>
+    /// 基本操作人
+    /// </summary>
+    public class BaseOperaror : IOperator
+    {
+        /// <summary>
+        /// 操作人id
+        /// </summary>
+        public string OperatorID { get; set; }
+
+        /// <summary>
+        /// 操作人名
+        /// </summary>
+        public string OperatorName { get; set; }
+    }
+}
