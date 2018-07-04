8b51650c269be9cd33d4163899a4cdee4adcfc92
weilai <raddleoj@sina.com>
2018-07-04 17:35:06 +0800

    文档和独立vue文件的实验

---
+++ WL.Core.WebApp/wwwroot/lib/component/my_component.vue
@@ -0,0 +1,22 @@
+﻿
+<template>
+<button class="my-c-class" v-on:click="count++">You clicked me {{ count }} times.333</button>
+</template>
+
+<script>
+    module.exports = {
+        data () {
+            return {
+                count: 0
+            }
+        }
+    }
+</script>
+
+
+<style scoped>
+.my-c-class{
+    background-color: black;
+    color: greenyellow;
+}
+</style>
