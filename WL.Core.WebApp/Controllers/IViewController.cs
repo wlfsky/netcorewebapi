8d5a3494f9566b180cc96e2dd5ce79a9ce49ec0f
wlfsky <raddleoj@sina.com>
2018-08-05 00:50:50 +0800

    app支持数据库

--- WL.Core.WebApp/Controllers/IViewController.cs
+++ WL.Core.WebApp/Controllers/IViewController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WL.Core.BusinessService;
+using WL.Core.Model;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,10 +12,24 @@ namespace WL.Core.WebApp.Controllers
 {
     public class IViewController : Controller
     {
+        private readonly IUserSystemBLL _userSystemBLL;
+
+        public IViewController(IUserSystemBLL userSystemBLL)
+        {
+            _userSystemBLL = userSystemBLL;
+        }
+
+
         // GET: /<controller>/
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult User()
+        {
+            var r = _userSystemBLL.GetList(new UserAccount());
+            return Json(r);
         }
     }
 }
---
+++ WlToolsLib/Expand/NumberExpand.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WlToolsLib.Expand
+{
+    /// <summary>
+    /// 数字类型的扩展
+    /// </summary>
+    public static class NumberExpand
+    {
+        #region --decimal --
+        /// <summary>
+        /// 金额数据格式化
+        /// </summary>
+        /// <param name="self"></param>
+        /// <returns></returns>
+        public static string ToMoney(this decimal self)
+        {
+            return self.ToString("f2");
+        } 
+        #endregion
+    }
+}
