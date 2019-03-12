42771ad669b5c6ae62b4e5cf5df2e3146401bb6a
wlfsky <raddleoj@sina.com>
2019-03-12 11:20:24 +0800

    加入一个h5子项目

---
+++ WL.H5App/WL.H5App/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import HttpClient from './common/httpclient';
+import Configs from '../config/index';
+
+export default {
+  name: 'App',
+  data(){
+    return {
+
+    };
+  },
+  created(){
+    let req = {id:100};
+    HttpClient.post('/do/something', req)
+    .then((res)=>{
+
+    })
+    .catch((err)=>{
+
+    });
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ WL.H5App/WL.H5App/src/config/index.js
@@ -0,0 +1,6 @@
+import httpConfig from "../config/httpConfig"
+
+
+export default {
+    HttpConfig: httpConfig,
+}
