a69199c4b36073a8ab7b2b48b7182dda549539e6
wlfsky <raddleoj@sina.com>
2019-07-12 09:48:16 +0800

    日志

---
+++ src/Core/WL.Core.Common/Log/ILog4.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WL.Core.Common
+{
+    public interface ILog4
+    {
+        void ErrorLog(string msg);
+        void DebugLog(string msg);
+        void InfoLog(string msg);
+    }
+}
