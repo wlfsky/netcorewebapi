c538ad98b5ed40688ffb3d211b77a5f79a1bb04a
weilai <raddleoj@sina.com>
2020-04-22 12:04:58 +0800

    追加两个忽略的文件

---
+++ src/Core/WL.Core.Common/Log/ILog.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WL.Core.Common
+{
+    public interface ILog
+    {
+        void ErrorLog(string msg);
+        void ErrorLog(Exception ex);
+        void DebugLog(string msg);
+        void DebugLog(Exception ex);
+        void InfoLog(string msg);
+        void InfoLog(Exception ex);
+    }
+}
---
+++ src/Core/WL.Core.Common/Log/SysLog.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace WL.Core.Common.Log
+{
+    public class SysLog
+    {
+        public static void ErrorLog(string msg)
+        {
+            DebugVS(msg);
+            ILog log = new Log4Api();
+            log.ErrorLog(msg);
+        }
+        public static void DebugLog(string msg)
+        {
+            DebugVS(msg);
+            ILog log = new Log4Api();
+            log.DebugLog(msg);
+        }
+        public static void InfoLog(string msg)
+        {
+            DebugVS(msg);
+            ILog log = new Log4Api();
+            log.InfoLog(msg);
+        }
+
+        public static void ErrorLog(Exception ex)
+        {
+            DebugVS(ex);
+            ILog log = new Log4Api();
+            log.ErrorLog(ex);
+        }
+        public static void DebugLog(Exception ex)
+        {
+            DebugVS(ex);
+            ILog log = new Log4Api();
+            log.DebugLog(ex);
+        }
+        public static void InfoLog(Exception ex)
+        {
+            DebugVS(ex);
+            ILog log = new Log4Api();
+            log.InfoLog(ex);
+        }
+
+        private static void DebugVS(string msg)
+        {
+            Debug.WriteLine(msg);
+        }
+
+        private static void DebugVS(Exception ex)
+        {
+            Debug.WriteLine(ExceptionFormat(ex));
+        }
+
+        private static string ExceptionFormat(Exception ex)
+        {
+            var msg = $"Msg: {ex.Message} -- StackTrace {ex.StackTrace}";
+            return msg;
+        }
+    }
+}
